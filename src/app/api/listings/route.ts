6e99b3dec3e2a14d0d0ec5e0817bf6eb60501dd7
momo-dev1 <didy1234567@gmail.com>
2023-09-17 18:07:58 +0300

    listing api

---
+++ src/app/api/listings/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import prismadb from "@/lib/prismadb"
+import getCurrentUser from "@/actions/getCurrentUser";
+
+export async function POST(req: Request) {
+
+    const currentUser = await getCurrentUser()
+
+    if (!currentUser) return NextResponse.error()
+
+    const body = await req.json()
+    const {
+        title,
+        price,
+        location,
+        category,
+        imageSrc,
+        roomCount,
+        guestCount,
+        description,
+        bathroomCount,
+    } = body
+
+    Object.keys(body).forEach(value => {
+        if (!body[value]) return NextResponse.error()
+    })
+
+    const listing = await prismadb.listing.create({
+        data: {
+            title,
+            category,
+            imageSrc,
+            roomCount,
+            guestCount,
+            description,
+            bathroomCount,
+            userId: currentUser.id,
+            price: parseInt(price, 10),
+            locationValue: location.value,
+        }
+    })
+
+    return NextResponse.json(listing)
+}
