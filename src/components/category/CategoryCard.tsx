3a5ef8b4b44cdad81d2ddcdce578268d652dfb41
momo-dev1 <didy1234567@gmail.com>
2023-09-18 18:45:00 +0300

    refactor category card

--- src/components/category/CategoryCard.tsx
+++ src/components/category/CategoryCard.tsx
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 import { slugify } from "@/utils/slugify";
 import { useSearchParams, useRouter } from "next/navigation";
 import queryString from "query-string";
 import { useCallback } from "react";
+import { IconType } from "react-icons";
 
-type CategoryCardProps = {
+interface CategoryCardProps {
+  icon: IconType;
   label: string;
-  srcImg: string;
-};
+  selected?: boolean;
+}
 
-const CategoryCard: React.FC<CategoryCardProps> = ({ label, srcImg }) => {
+const CategoryCard: React.FC<CategoryCardProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
   const params = useSearchParams();
   const router = useRouter();
 
@@ -39,8 +45,25 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ label, srcImg }) => {
   }, [label, params, router]);
 
   return (
-    <div onClick={handleClick} className="rounded-lg">
-      CategoryCard
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? "border-b-neutral-800" : "border-transparent"}
+        ${selected ? "text-neutral-800" : "text-neutral-500"}
+      `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
     </div>
   );
 };
