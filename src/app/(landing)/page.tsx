f2c269fda3ffad5b271fe378a960435cf2e44b9e
momo-dev1 <didy1234567@gmail.com>
2023-09-25 11:02:17 +0300

    refactoring home

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -14,36 +14,35 @@ const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
-  if (listings.length === 0) {
-    return <EmptyState showReset />;
-  }
-
   return (
     <Container>
       <main className='min-h-screen'>
         <Hero />
-
-        <div
-          className='
-            grid
-            grid-cols-1 
-            gap-8 
-            pt-24 
-            sm:grid-cols-2 
-            md:grid-cols-3
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-          '
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
+        {listings.length === 0 ? (
+          <EmptyState showReset />
+        ) : (
+          <div
+            className='
+    grid
+    grid-cols-1 
+    gap-8 
+    pt-24 
+    sm:grid-cols-2 
+    md:grid-cols-3
+    lg:grid-cols-4
+    xl:grid-cols-5
+    2xl:grid-cols-6
+  '
+          >
+            {listings.map((listing: any) => (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            ))}
+          </div>
+        )}
       </main>
     </Container>
   );
