6ec766ae1d085e260652beca094fc091a462639b
momo-dev1 <didy1234567@gmail.com>
2023-10-01 14:04:52 +0300

    reservation functionality

--- src/components/listings/ListingReservation.tsx
+++ src/components/listings/ListingReservation.tsx
@@ -36,7 +36,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
     >
       <div className=' flex flex-row items-center gap-1 p-4'>
         <div className='text-2xl font-semibold'>$ {price}</div>
-        <div className='font-light text-neutral-600'>night</div>
+        <div className='font-light text-neutral-600'>/ night</div>
       </div>
       <hr />
       <Calendar
@@ -46,7 +46,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
       />
       <hr />
       <div className='p-4'>
-        <Button disabled={disabled} onClick={onSubmit}>
+        <Button className='flex-1' disabled={disabled} onClick={onSubmit}>
           Reserve
         </Button>
       </div>
